300f2b812eae2ad865cea558cfc7ca683e58fc84
toluw <oketoluwase@gmail.com>
2022-03-18 20:18:05 +0100

    Check if device is AR compatible

--- app/src/main/java/com/tech/camx/ui/MainActivity.kt
+++ app/src/main/java/com/tech/camx/ui/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.tech.camx.ui
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.tech.camx.R
+import com.tech.camx.utils.MIN_OPENGL_VERSION
+import com.tech.camx.utils.TAG
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkIsSupportedDeviceOrFinish(this)
+
         setContentView(R.layout.activity_main)
+    }
+
+    private fun checkIsSupportedDeviceOrFinish(activity: Activity){
+
+        val openGlVersionString =
+            (Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)) as ActivityManager)
+                .deviceConfigurationInfo
+                .glEsVersion
+
+        if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
+            Toast.makeText(activity, "Unsupported device", Toast.LENGTH_LONG)
+                .show()
+            activity.finish()
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/tech/camx/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.tech.camx.utils
+
+const val MIN_OPENGL_VERSION = 3.0
+const val TAG = "App_Tag"
